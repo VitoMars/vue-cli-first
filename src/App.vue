a461f7c5380bff2660794ddbdf144e3ec8af14fd
Vito <vito95v@gmail.com>
2021-09-17 18:15:30 +0200

    Fine esercizio + bonus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="app">
+      <titolo msg="QUACK" />
+
+      <img src="./assets/img/duck.jpg" alt="" />
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import titolo from "./components/Quack.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   name: "App",
+   components: {
+      titolo,
+   },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
 }
 </style>
